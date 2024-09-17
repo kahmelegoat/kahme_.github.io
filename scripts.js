a71c1af856b84499ae0cd10488c2e313b66fd67b
kahme <157324720+kahmelegoat@users.noreply.github.com>
2024-09-17 18:43:51 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,30 +1,27 @@
-// Basculer pour le mot de passe principal
-const togglePassword = document.querySelector('#togglePassword');
-const passwordInput = document.querySelector('#password');
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
 
-togglePassword.addEventListener('click', function () {
-    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-    passwordInput.setAttribute('type', type);
-    this.classList.toggle('fa-eye-slash');
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
 });
 
-// Basculer pour le mot de passe de confirmation
-const toggleConfirmPassword = document.querySelector('#toggleConfirmPassword');
-const confirmPasswordInput = document.querySelector('#confirmPassword');
-
-toggleConfirmPassword.addEventListener('click', function () {
-    const type = confirmPasswordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-    confirmPasswordInput.setAttribute('type', type);
-    this.classList.toggle('fa-eye-slash');
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
 });
-const termsCheckbox = document.querySelector('#termsCheckbox');
-const signupButton = document.querySelector('#signupButton');
 
-// Désactive le bouton si la case n'est pas cochée
-termsCheckbox.addEventListener('change', function () {
-    if (termsCheckbox.checked) {
-        signupButton.disabled = false;
-    } else {
-        signupButton.disabled = true;
-    }
+// Cibler le lien et la section
+const forgotPasswordLink = document.getElementById('forgot-password-link');
+const forgotPasswordSection = document.getElementById('forgot-password');
+const toggleContainer = document.getElementById('toggle-container');
+
+// Ajouter un gestionnaire d'événement pour le clic
+forgotPasswordLink.addEventListener('click', function(e) {
+    e.preventDefault(); // Empêcher le comportement par défaut du lien
+
+    // Activer l'animation de la section "Mot de passe oublié"
+    forgotPasswordSection.classList.add('active'); 
+
+    // Déplacer le panneau bleu vers le haut
+    toggleContainer.classList.add('move-up');
 });
